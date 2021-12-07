58c71936d3aa60a1b852e4d32275c20a8087318f
= <liam2robertson@gmail.com>
2021-12-07 23:41:07 +0000

    💄 add Work Sans font

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<main>
+  <slot />
+</main>
+
+<style global lang="scss">
+  @font-face {
+    font-family: "Work Sans";
+    src: url("/font/WorkSans-Subset.woff2") format("woff2");
+    font-style: normal;
+    font-weight: 1 999;
+    font-display: swap;
+  }
+
+  body {
+    font-family: 'Work Sans', sans-serif;
+  }
+</style>
