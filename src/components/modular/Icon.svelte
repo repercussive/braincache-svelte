81c7e5f69279160cbe9a816a9d2e3e4cb249d824
= <liam2robertson@gmail.com>
2021-12-09 11:56:28 +0000

    💄 add Icon component

---
+++ src/components/modular/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { SvelteComponent } from 'svelte'
+  export let icon: typeof SvelteComponent
+  export let size = 1
+  export let color = 'text'
+  export let style = ''
+
+  $: allStyles = `font-size: ${size}rem; color:var(--color-${color});` + style
+</script>
+
+<div style={allStyles}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="1em"
+    height="1em"
+    fill="none"
+  >
+    <svelte:component this={icon} />
+  </svg>
+</div>
