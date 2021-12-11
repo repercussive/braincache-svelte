d3ee44b8e1c2c3ddfda65ce5f6f2609bf9bbd3df
= <liam2robertson@gmail.com>
2021-12-11 21:13:21 +0000

    💄 use component map to determine displayed screen

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
+  import { SvelteComponent } from 'svelte'
   import appScreen, { AppScreen } from '$logic/app/appScreen'
   import WelcomeScreen from '$components/screens/WelcomeScreen.svelte'
   import GameScreen from '$components/screens/GameScreen.svelte'
   import EndScreen from '$components/screens/EndScreen.svelte'
 
-  $: component = getScreen($appScreen)
-
-  function getScreen(screen: AppScreen) {
-    switch (screen) {
-      case 'welcome':
-        return WelcomeScreen
-      case 'game':
-        return GameScreen
-      case 'game-end':
-        return EndScreen
-    }
+  const componentMap: Record<AppScreen, typeof SvelteComponent> = {
+    'welcome': WelcomeScreen,
+    'game': GameScreen,
+    'game-end': EndScreen,
   }
+
+  $: component = componentMap[$appScreen]
 </script>
 
 <svelte:component this={component} />
